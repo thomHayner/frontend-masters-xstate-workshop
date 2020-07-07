023280544f1847d13ac6bf6d954988072a9c583a
thomHayner <thom.hayner@gmail.com>
2020-07-07 11:33:51 -0600

    define machine

--- src/02/index.js
+++ src/02/index.js
@@ -4,10 +4,23 @@ const elBox = document.querySelector('#box');
 
 const machine = createMachine({
   // Add your object machine definition here
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        CLICK: 'active'
+      }
+    },
+    active: {
+      on: {
+        CLICK: 'inactive'
+      }
+    }
+  }
 });
 
 // Change this to the initial state
-let currentState = undefined;
+let currentState = machine.initial;
 
 function send(event) {
   // Determine and update the `currentState`
