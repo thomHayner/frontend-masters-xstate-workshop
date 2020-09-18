5fd27a5a25b890ec5f04ff6846aaa20655609ad4
thomHayner <thom.hayner@gmail.com>
2020-09-18 10:17:10 -0600

    lesson 01

--- src/01/index.js
+++ src/01/index.js
@@ -2,24 +2,60 @@ const elBox = document.querySelector('#box');
 
 // Pure function that returns the next state,
 // given the current state and sent event
-function transition(state, event) {
-  switch (
-    state
-    // Add your state/event transitions here
-    // to determine and return the next state
-  ) {
+// function transition(state, event) {
+//   switch (state) {
+//     case 'inactive':
+//       switch (event) {
+//         case 'CLICK':
+//           return 'active';
+//         default:
+//           return state;
+//       }
+//     case 'active':
+//       switch (event) {
+//         case 'CLICK':
+//           return 'inactive';
+//         default:
+//           return state;
+//       }
+//     default:
+//       return state;
+//   }
+// }
+
+const machine = {
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        CLICK: 'active',
+      },
+    },
+    active: {
+      on: {
+        CLICK: 'inactive',
+      },
+    },
   }
 }
 
 // Keep track of your current state
-let currentState = undefined;
+let currentState = machine.initial;
+
+function transition(state, event) {
+  const nextState = machine.states[state].on?.[event] || state;
+  
+  return nextState;
+}
 
 function send(event) {
   // Determine the next value of `currentState`
+  currentState = transition(currentState, event)
 
   elBox.dataset.state = currentState;
 }
 
 elBox.addEventListener('click', () => {
   // send a click event
+  send('CLICK');
 });
